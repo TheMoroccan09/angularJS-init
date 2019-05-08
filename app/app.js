685355ca364f61f483644416c326467449338e62
Othmane Mohimi <otmohimi@everis.com>
2019-05-08 14:54:56 +0000

    rounting with !#

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 angular
     .module('App', ['ngRoute'])
-    .config(function($routeProvider){
+    .config(function($routeProvider, $locationProvider){
         $routeProvider
           .when('/', {
             templateUrl: '../templates/pages/home.html',
             controller: 'HomeController'
           })
+          .when('/About', {
+            templateUrl: '../templates/pages/about.html',
+            controller: 'HomeController'
+          })
+          .when('/Contact', {
+            templateUrl: '../templates/pages/contact.html',
+            controller: 'HomeController'
+          })
           .otherwise({
             templateUrl: '../templates/pages/404.html'
-          })
+          });
+
+        // $locationProvider.html5Mode(true);
     });
